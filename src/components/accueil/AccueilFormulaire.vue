9d5e77af7237c57e11e09900de19e7bdc7c2c099
Glenou <glen@yeswedev.fr>
2021-10-06 19:53:19 +0200

    Setting up the Hero

--- src/components/accueil/AccueilFormulaire.vue
+++ src/components/accueil/AccueilFormulaire.vue
@@ -1,5 +1,49 @@
 <template>
   <div>
-    COUCOU FORMULAIRE
+    <form
+      data-netlify="true"
+      name="contact"
+      method="POST"
+      class="accueil-formulaire"
+    >
+      <h3>être recontacté(e)* :</h3>
+
+      <label for="prenom" class="accueil-formulaire-label">
+        <span>Prénom *</span>
+        <input type="text" name="prenom" required>
+      </label>
+
+      <label for="nom" class="accueil-formulaire-label">
+        <span>Nom *</span>
+        <input type="text" name="nom" required>
+      </label>
+
+      <label for="societe" class="accueil-formulaire-label">
+        <span>Société *</span>
+        <input type="text" name="societe" required>
+      </label>
+
+      <label for="email" class="accueil-formulaire-label">
+        <span>Email *</span>
+        <input type="email" name="email" required>
+      </label>
+
+      <label for="quantite" class="accueil-formulaire-label">
+        <span>Quantité désirée *</span>
+        <input type="number" name="quantite" min="1" required>
+      </label>
+
+      <p class="text-xs">
+        * Champs obligatoires
+      </p>
+
+      <button type="submit" class="accueil-formulaire-button">
+        Envoyer
+      </button>
+
+      <p class="text-xs italic mt-3">
+        Ces informations ne sont pas communiquées à des tiers.
+      </p>
+    </form>
   </div>
 </template>
--- src/components/accueil/AccueilHero.vue
+++ src/components/accueil/AccueilHero.vue
@@ -1,5 +1,53 @@
 <template>
-  <div>
-    COUCOU HERO
-  </div>
+  <section class="accueil-hero">
+    <div class="accueil-hero-container ">
+      <Container class="lg:flex pt-16">
+        <div class="accueil-hero-text p-6">
+          <img
+            src="~assets/images/cfdome_logo_blanc.svg"
+            alt="Logotype de CF Group"
+            class="w-full mb-4 max-w-sm xl:max-w-md"
+          >
+          <h1 class="accueil-hero-title">
+            <span class="sr-only">CFDome</span>
+            l'abri gonflable pour <br>
+            chantier de piscine.
+          </h1>
+          <p class="accueil-hero-subtitle">
+            Que ce soit en construction, ou en rénovation, CFDome sécurise votre planning et assure votre trésorerie. On vous parie que vous vous demanderez comment vous avez travaillé sans jusqu'ici !
+          </p>
+        </div>
+        <div class="p-6">
+          <AccueilFormulaire />
+        </div>
+      </Container>
+      <div class="accueil-hero-sidebar-badges">
+        <div class="accueil-hero-sidebar-badges-container lg:pt-16">
+          <div class="badge badge--shadow">
+            <img src="~assets/images/fabrication-europeenne.svg" alt="Badge de la fabrication européenne">
+          </div>
+          <div class="badge badge--shadow">
+            <img src="~assets/images/systeme-brevete.svg" alt="Badge du système breveté">
+          </div>
+          <div class="cf-group-logo lg:mt-auto">
+            <p class="w-full text-center mb-1 text-lg">
+              un produit
+            </p>
+            <img src="~assets/images/cf-group-logo.svg" alt="Logotype de CF Group">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Container from '../shared/layouts/Container.vue';
+
+export default Vue.extend({
+  components: {
+    Container,
+  },
+});
+</script>

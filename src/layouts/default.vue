dfd728280ba0e22b3dd4678866c3c93e9c7ccb6f
Glenou <glen@yeswedev.fr>
2021-10-08 15:08:18 +0200

    Debugging success modal

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -9,16 +9,16 @@ import Vue from 'vue';
 
 export default Vue.extend({
   mounted() {
-    window.axeptioSettings = {
-      clientId: '615d62eb6e2df966fcd2f92a',
-    };
+    // window.axeptioSettings = {
+    //   clientId: '615d62eb6e2df966fcd2f92a',
+    // };
 
-    (function (d, s) {
-      var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
-      e.async = true;
-      e.src = "//static.axept.io/sdk.js";
-      t.parentNode.insertBefore(e, t);
-    })(document, "script");
+    // (function (d, s) {
+    //   var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+    //   e.async = true;
+    //   e.src = "//static.axept.io/sdk-slim.js";
+    //   t.parentNode.insertBefore(e, t);
+    // })(document, "script");
   },
 });
 </script>

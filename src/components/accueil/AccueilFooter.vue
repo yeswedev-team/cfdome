eaa2402953e16b2fb3641432fc7f5a3a3cb6e848
JohnnyYWD <63596130+JohnnyYWD@users.noreply.github.com>
2023-11-15 19:02:09 +0100

    Update AccueilFooter.vue

--- src/components/accueil/AccueilFooter.vue
+++ src/components/accueil/AccueilFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="accueil-footer text-sm">
     <p class="mb-2">
-      *En soumettant ce formulaire de contact sur notre site, vous convenez et acceptez que DEL SAS puisse recueillir et traiter les données personnelles soumises, en conformité avec les règles exposées dans notre <a href="https://www.my-cfgroup.fr/fr/mentions-legales" target="_blank" class="underline">Politique de confidentialité</a>.
+      *En soumettant ce formulaire de contact sur notre site, vous convenez et acceptez que DEL SAS puisse recueillir et traiter les données personnelles soumises, en conformité avec les règles exposées dans notre <a href="https://www.my-cfgroup.fr/fr/mentions-legales-2" target="_blank" class="underline">Politique de confidentialité</a>.
     </p>
     <p>
       **Ce tarif correspond à notre abri de chantier CF Dome en taille L, tarif au 17 mars 2023, sur une durée de 36 mois. Sous réserve de validation de notre partenaire GRENKE LOCATION. Vous serez propriétaire de votre matériel, après paiement de l’intégralité des échéances . Le choix de la durée de location (18 à 36 mois) permet d’adapter vos mensualités aux capacités de remboursement de votre entreprise. Un crédit vous engage et doit être remboursé. Vérifiez vos capacités de remboursement avant de vous engager. © DEL SAS, Studio 3 Elements. Tous droits réservés. Reproduction interdite. Dreams, health & care with water = Concilier rêves, santé et soin grâce à l’eau.
